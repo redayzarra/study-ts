bceb25183c1c7594418afe90fbfdb505874c2488
ReDay Zarra <redayzarra@gmail.com>
2025-10-03 20:11:52 -0700

    Configuring debugger settings for typescript

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello");
+let age = 20;
+if (age <= 50) {
+    age += 10;
+}
+console.log(age);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello")
+let age: number = 20;
+
+if (age <= 50) {
+    age += 10
+}
+
+console.log(age)
